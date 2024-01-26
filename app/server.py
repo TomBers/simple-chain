0fa33260e711766783853b8604edeb2a894fab1d
TomBers <tjcberman@gmail.com>
2024-01-26 10:20:00 +0000

    use Jinja templates

--- app/server.py
+++ app/server.py
@@ -1,24 +1,58 @@
 from fastapi import FastAPI, Form
+from fastapi.templating import Jinja2Templates
 from typing import Annotated
-from fastapi.staticfiles import StaticFiles
+
 
 from simple_chain import chain as simple_chain
+
+
+from fastapi.requests import Request
+from fastapi.responses import HTMLResponse
+
 import random
 import time
 
 
 app = FastAPI()
 
-
-@app.post("/post_text")
-def post_text(myTextArea: Annotated[str, Form()]):
-    return simple_chain.invoke({"text": myTextArea})
-    # time.sleep(2)
-    # answer = "I don't know"
-    # return answer + " " + random.randint(1, 29) * "🤖"
+templates = Jinja2Templates(
+    directory="pages", autoescape=True, auto_reload=True)
 
 
-app.mount("/", StaticFiles(directory="static", html=True), name="app")
+@app.get("/", response_class=HTMLResponse)
+async def homepage(request: Request):
+    return templates.TemplateResponse(
+        name="index.html",
+        context={
+            "request": request
+        },
+    )
+
+
+@app.get("/demo", response_class=HTMLResponse)
+async def homepage(request: Request):
+    return templates.TemplateResponse(
+        name="demo.html",
+        context={
+            "request": request
+        },
+    )
+
+
+@app.post("/post_text", response_class=HTMLResponse)
+def post_text(request: Request, myTextArea: Annotated[str, Form()], myTextInput: Annotated[str, Form()] = ""):
+    # return simple_chain.invoke({"text": myTextArea})
+
+    return templates.TemplateResponse(
+        name="response.html",
+        context={
+            "request": request,
+            "question": myTextArea,
+            "input": myTextInput,
+            "answer": random.randint(1, 29) * "🤖"
+        },
+    )
+
 
 if __name__ == "__main__":
     import uvicorn
